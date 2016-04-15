a0435dee22630e83bbefdb13d4119e555f4a9236
abel deng <abeldeng@qq.com>
2016-04-15 12:43:27 +0800

    demo create

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+var http = require('http')
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  })
+  res.end('Hello World!')
+})
+
+server.listen(80, function() {
+  console.log('Docker DEMO with Node.js is running.')
+})
