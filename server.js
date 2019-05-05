630c398c6f7fc8f4a115cf430232e22ddf63e3f3
wanglong167 <wang_.long@qq.com>
2019-05-05 14:13:51 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-// server.js
-var express = require('express');
+const express = require('express');
+
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World! from ' + process.env.HOSTNAME);
+app.get('/', (req, res) => {
+    res.status(200).send({
+        name: 'John',
+        age: 26
+    });
 });
 
-var server = app.listen(3000, function () {
-  console.log('Server started');
+app.listen(3000, () => {
+    console.log("Server started at port 3000");
 });
+
+module.exports.app = app;
